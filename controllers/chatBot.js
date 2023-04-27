38b0b8f441162d99035a81bf62b91f444ae96b1c
Chinmay-S <chinmay.subramanya@gmail.com>
2023-04-27 21:12:41 +0530

    Final

--- controllers/chatBot.js
+++ controllers/chatBot.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: 'https://api.openai.com/v1/chat/completions',
-  headers: {
-    'Authorization' : process.env.CHAT_GPT_KEY,//'Bearer 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',
-    'Content-Type' : 'application/json',
-  },
-});
-export async function getMessage(req,res) {
+
+export const getMessage = async (req, res) => {console.log(req.body)
+    const instance = axios.create({
+        baseURL: 'https://api.openai.com/v1/chat/completions',
+        headers: {
+          'Authorization' : 'Bearer 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',//'Bearer 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',
+          'Content-Type' : 'application/json',
+        },
+      });
+      
+      
+      let resp = "error";
+      console.log("executed")
     await instance({
       method: 'POST',
       data: {
@@ -18,11 +23,13 @@ export async function getMessage(req,res) {
     }).then(response => {
       console.log(response.data);
       console.log("Executed");
-      const op = response['data']['choices'][0]['message']['content'];
-      console.log(op);
-      res.status(200).json({op})
+      resp = response['data']['choices'][0]['message']['content'];
+      //console.log(op);
+    //  res.status(200).json({response})
+      
     }).catch(error => {
       console.log("Error");
-      res.status(200).json({error})
+      // res.status(401).json({error})
     });
+    res.status(200).json({resp})
 }
