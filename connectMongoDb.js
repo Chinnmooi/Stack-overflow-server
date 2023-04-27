a45945626041fd24d956872fe97a92ffb727ba9c
ChinmayS001 <63219855+ChinmayS001@users.noreply.github.com>
2023-04-27 11:23:59 +0530

    Update connectMongoDb.js

--- connectMongoDb.js
+++ connectMongoDb.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    const conn =    process.env.MONGODB_CONNECTION_URL  ||  "mongodb+srv://admin:admin@cluster0.gygecir.mongodb.net/test"
+    console.log(process.env.MONGODB_CONNECTION_URL)
     const conn = await mongoose.connect("mongodb+srv://admin:admin@cluster0.gygecir.mongodb.net/test", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
